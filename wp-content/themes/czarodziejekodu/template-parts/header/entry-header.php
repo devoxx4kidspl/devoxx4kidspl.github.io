f03bf6e288b19e4c3fa9e011b31a516352e05c44
Andrzej 'nAndy' Łukaszewski <lukaszewski.a@gmail.com>
2019-03-27 01:03:48 +0100

    remove title for main page and style divs

--- wp-content/themes/czarodziejekodu/template-parts/header/entry-header.php
+++ wp-content/themes/czarodziejekodu/template-parts/header/entry-header.php
@@ -9,7 +9,11 @@
 
 $discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null; ?>
 
-<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<?php
+if ( !is_front_page() ) {
+	the_title( '<h1 class="entry-title">', '</h1>' );
+}
+?>
 
 <?php if ( ! is_page() ) : ?>
 <div class="entry-meta">
